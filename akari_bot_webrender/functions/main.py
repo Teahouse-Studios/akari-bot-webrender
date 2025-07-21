69ea90407526f4f5206520a289d0c4c7a285fcbf
yzhh <31803608+OasisAkari@users.noreply.github.com>
2025-07-21 14:35:13 +0800

    split javascript function to separate files

--- akari_bot_webrender/functions/main.py
+++ akari_bot_webrender/functions/main.py
@@ -261,31 +261,7 @@ class WebRender:
         )
 
         # :rina: :rina: :rina: :rina:
-        await page.evaluate("""(elements_to_disable) => {
-            const images = document.querySelectorAll("img")
-            images.forEach(image => {
-              image.removeAttribute('loading');
-            })
-            const animated = document.querySelectorAll(".animated")
-            for (var i = 0; i < animated.length; i++) {
-              b = animated[i].querySelectorAll('img')
-              for (ii = 0; ii < b.length; ii++) {
-                b[ii].width = b[ii].getAttribute('width') / (b.length / 2)
-                b[ii].height = b[ii].getAttribute('height') / (b.length / 2)
-              }
-              animated[i].className = 'nolongeranimatebaka'
-            }
-            for (var i = 0; i < elements_to_disable.length; i++) {
-              const element_to_boom = document.querySelector(elements_to_disable[i])// :rina: :rina: :rina: :rina:
-              if (element_to_boom != null) {
-                element_to_boom.style = 'display: none'
-              }
-            }
-            document.querySelectorAll('*').forEach(element => {
-              element.parentNode.replaceChild(element.cloneNode(true), element);
-            });
-            window.scroll(0, 0)
-              }""", elements_to_disable)
+        await page.evaluate(open(templates_path + '/element_screenshot_evaluate.js').read(), elements_to_disable)
         images = await self.select_element_and_screenshot(
             elements=options.element,
             page=page,
@@ -307,69 +283,7 @@ class WebRender:
             css=options.css,
             stealth=options.stealth
         )
-        await page.evaluate("""({section, elements_to_disable}) => {
-            console.log("Section: " + section)
-            console.log("Elements to disable: " + elements_to_disable)
-            
-            const levels = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6']
-            let sec = document.getElementById(section).parentNode
-            const sec_level = sec.tagName
-            if (sec.parentNode.className.includes('ext-discussiontools-init-section')) { // wo yi ding yao sha le ni men
-              sec = sec.parentNode
-            }
-            const nbox = document.createElement('div')
-            nbox.className = 'bot-sectionbox'
-            nbox.style = 'display: inline-block'
-            nbox.appendChild(sec.cloneNode(true))
-            let next_sibling = sec.nextSibling
-            while (next_sibling) {
-              if (levels.includes(next_sibling.tagName)) {
-                if (levels.indexOf(next_sibling.tagName) <= levels.indexOf(sec_level)) {
-                  break
-                }
-              }
-              if (next_sibling.tagName == 'DIV' && next_sibling.className.includes('ext-discussiontools-init-section')) { // wo yi ding yao sha le ni men
-                let child = next_sibling.firstChild
-                let bf = false
-                while (child) {
-                  if (levels.includes(child.tagName)) {
-                    if (levels.indexOf(child.tagName) <= levels.indexOf(sec_level)) {
-                      bf = true
-                      break
-                    }
-                  }
-                  child = child.nextSibling
-                }
-                if (bf) {
-                  break
-                }
-    
-    
-              }
-              nbox.appendChild(next_sibling.cloneNode(true))
-              next_sibling = next_sibling.nextSibling
-            }
-            const lazyimg = nbox.querySelectorAll(".lazyload")
-            for (var i = 0; i < lazyimg.length; i++) {
-              lazyimg[i].className = 'image'
-              lazyimg[i].src = lazyimg[i].getAttribute('data-src')
-            }
-            const new_parentNode = sec.parentNode.cloneNode()
-            const pparentNode = sec.parentNode.parentNode
-            pparentNode.removeChild(sec.parentNode)
-            pparentNode.appendChild(new_parentNode)
-            new_parentNode.appendChild(nbox)
-            for (var i = 0; i < elements_to_disable.length; i++) {
-              const element_to_boom = document.querySelector(elements_to_disable[i])// :rina: :rina: :rina: :rina:
-              if (element_to_boom != null) {
-                element_to_boom.style = 'display: none'
-              }
-            }
-            document.querySelectorAll('*').forEach(element => {
-              element.parentNode.replaceChild(element.cloneNode(true), element);
-            });
-            window.scroll(0, 0)
-          }""", {"section": options.section, "elements_to_disable": elements_to_disable})
+        await page.evaluate(open(templates_path + '/section_screenshot_evaluate.js').read(), {"section": options.section, "elements_to_disable": elements_to_disable})
         images = await self.select_element_and_screenshot(
             elements='.bot-sectionbox',
             page=page,
