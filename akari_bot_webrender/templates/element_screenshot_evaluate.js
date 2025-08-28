4749c3c3d3b7e9187a3a99189d57358be24475a6
多羅狼 <Dorowolf314@gmail.com>
2025-08-29 04:22:37 +0800

    wtf

--- akari_bot_webrender/templates/element_screenshot_evaluate.js
+++ akari_bot_webrender/templates/element_screenshot_evaluate.js
@@ -1,25 +1,29 @@
 function element_screenshot_evaluate(elements_to_disable) {
-    const images = document.querySelectorAll("img")
-    images.forEach(image => {
-        image.removeAttribute('loading');
-    })
-    const animated = document.querySelectorAll(".animated")
-    for (var i = 0; i < animated.length; i++) {
-        b = animated[i].querySelectorAll('img')
-        for (ii = 0; ii < b.length; ii++) {
-            b[ii].width = b[ii].getAttribute('width') / (b.length / 2)
-            b[ii].height = b[ii].getAttribute('height') / (b.length / 2)
-        }
-        animated[i].className = 'nolongeranimatebaka'
+  const images = document.querySelectorAll("img");
+  images.forEach((image) => {
+    image.removeAttribute("loading");
+  });
+
+  const animated = document.querySelectorAll(".animated");
+  for (let i = 0; i < animated.length; i++) {
+    const b = animated[i].querySelectorAll("img");
+    for (let ii = 0; ii < b.length; ii++) {
+      b[ii].width = b[ii].getAttribute("width") / (b.length / 2);
+      b[ii].height = b[ii].getAttribute("height") / (b.length / 2);
     }
-    for (var i = 0; i < elements_to_disable.length; i++) {
-        const element_to_boom = document.querySelector(elements_to_disable[i])// :rina: :rina: :rina: :rina:
-        if (element_to_boom != null) {
-            element_to_boom.style = 'display: none !important'
-        }
+    animated[i].className = "nolongeranimatebaka";
+  }
+
+  for (let i = 0; i < elements_to_disable.length; i++) {
+    const element_to_boom = document.querySelector(elements_to_disable[i]); // :rina: :rina: :rina: :rina:
+    if (element_to_boom != null) {
+      element_to_boom.style = "display: none !important";
     }
-    document.querySelectorAll('*').forEach(element => {
-        element.parentNode.replaceChild(element.cloneNode(true), element);
-    });
-    window.scroll(0, 0)
+  }
+
+  document.querySelectorAll("*").forEach((element) => {
+    element.parentNode.replaceChild(element.cloneNode(true), element);
+  });
+
+  window.scroll(0, 0);
 }
